95c07b85b773d87491957e9d7edc2802c3c7f28d
Cristian <tironi18@gmail.com>
2022-07-30 11:08:17 +0200

    update

--- hack_bz/lib/main.dart
+++ hack_bz/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Hackaton BZ'),
+      home: const myHomePage(title: 'Hackaton BZ'),
     );
   }
 }
